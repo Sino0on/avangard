9cfac845bece3bba31f3891603fbd57c9ccf55ba
Dastan <dastan.kubanuulu1@gmail.com>
2024-12-01 23:44:15 +0600

    add new permessions

--- objects/views.py
+++ objects/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import *
 from .serializers import *
 
 
 class BuildingListApiView(generics.ListAPIView):
+    permission_classes = [AllowAny]
     serializer_class = BuildingListSerializer
-    queryset = Building
+    queryset = Building.objects.all()
 
 
 class BuildingDetailApiView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
     serializer_class = BuildingSerializer
-    queryset = Building
+    queryset = Building.objects.all()
     lookup_field = 'pk'
 
