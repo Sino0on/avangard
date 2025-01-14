a8525a33ac916f31e95bbbd0a669ba50da0fd200
Dastan <dastan.kubanuulu1@gmail.com>
2025-01-14 21:33:58 +0600

    das

--- objects/views.py
+++ objects/views.py
@@ -1,7 +1,13 @@
 from django.http import Http404
 from django.shortcuts import render
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
+from unicodedata import category
+from yaml import safe_load
+
 from .models import *
 from .serializers import *
 
@@ -19,17 +25,41 @@ class BuildingDetailApiView(generics.RetrieveAPIView):
     lookup_field = 'slug'
 
     def get_queryset(self):
+        category_id = self.request.GET.get('category_id')
         slug = self.kwargs.get('slug')
-        queryset = Building.objects.filter(slug=slug)
+        if category_id:
+            queryset = Building.objects.filter(slug=slug, category__id=category_id)
+        else:
+            queryset = Building.objects.filter(slug=slug)
         if not queryset.exists():
             raise Http404("Категория не существует")
         return queryset
 
-
+@extend_schema_view(
+    get=extend_schema(
+        description='Категория айди',
+        summary='вфыефт',
+        parameters=[
+            OpenApiParameter(name='category_id',
+                             description='ID Категория',
+                             type=OpenApiTypes.INT, required=True),
+        ]
+    ),
+)
 class BuildingEndedApiView(generics.ListAPIView):
     permission_classes = (AllowAny,)
     serializer_class = BuildingListSerializer
     queryset = Building.objects.filter(status='ended')
+
+    def get_queryset(self):
+        category_id = self.request.GET.get('category_id')
+        if category_id:
+            queryset = Building.objects.filter(status='ended', category__id=category_id)
+        else:
+            queryset = Building.objects.filter(status='ended')
+        if not queryset.exists():
+            raise Http404("Категория не существует")
+        return queryset
 
 
 class BuildingEndedDetailApiView(generics.RetrieveAPIView):
