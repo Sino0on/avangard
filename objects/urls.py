38f92be1acfde730961e645cbd0bce6e1a4e4b91
Dastan <dastan.kubanuulu1@gmail.com>
2025-02-01 12:21:24 +0600

    das

--- objects/urls.py
+++ objects/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('list/', BuildingListApiView.as_view()),
-    path('list/home/', ObjectsForHomeApiView.as_view()),
+    path('list/3d/', ObjectsForHomeApiView.as_view()),
     path('detail/<slug:slug>/', BuildingDetailApiView.as_view()),
     path('detail/3d/<slug:slug>/', BuildingThreeDDetailApiView.as_view()),
     path('list/ended/', BuildingEndedApiView.as_view()),
